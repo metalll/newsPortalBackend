fec165b97ca0686adbac3dc7f26a63b9f08e7a30
NSD <metall112@bk.ru>
2017-03-05 16:26:54 +0200

    NewsPortal/`

--- src/main/java/launch/Main.java
+++ src/main/java/launch/Main.java
@@ -50,7 +50,7 @@ public class Main {
         }
 
         tomcat.setPort(Integer.valueOf(webPort));
-        File webContentFolder = new File(root.getAbsolutePath(), "src/main/webapp/");
+        File webContentFolder = new File(root.getAbsolutePath(), "src/main/webapp/NewsPortal/");
         if (!webContentFolder.exists()) {
             webContentFolder = Files.createTempDirectory("default-doc-base").toFile();
         }
