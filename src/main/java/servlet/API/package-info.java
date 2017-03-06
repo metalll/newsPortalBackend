b63990284472581ca84760dc755766abdc82912d
NSD <metall112@bk.ru>
2017-03-06 22:29:03 +0200

    pre relise basic functional created

---
+++ src/main/java/servlet/API/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Created by NSD on 06.03.17.
+ */
+package servlet.API;
+/* Название говорит само за себя. Пакет содержит все контроллеры для работы с моделью
+ * по принципу CRUD (Create Read Update Delete) где post у каждого сервелета - Create+Update
+  * get - Read и delete - delete(тавтология но щито поделать :-\) */
--- src/main/java/servlet/package-info.java
+++ src/main/java/servlet/package-info.java
@@ -2,3 +2,6 @@
  * Created by NSD on 06.03.17.
  */
 package servlet;
+
+/* функция -  Web controllers то биш BL (bisness logic) */
+/* Содержит все сервлеты используемые в проекте */
