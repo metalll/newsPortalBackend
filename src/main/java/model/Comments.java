5bc1f5b31f7deb612350d6fd8d418c1ede518bbe
NSD <metall112@bk.ru>
2017-03-06 14:18:30 +0200

    fix coment model

--- src/main/java/model/Comments.java
+++ src/main/java/model/Comments.java
@@ -15,7 +15,7 @@ public class Comments {
     public Comments(){}
 
     @DatabaseField(generatedId = true)
-    private String id;
+    private int id;
     @DatabaseField
     private String annonymus;
     @DatabaseField
@@ -28,11 +28,11 @@ public class Comments {
     private String comment;
 
 
-    public String getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(int id) {
         this.id = id;
     }
 
