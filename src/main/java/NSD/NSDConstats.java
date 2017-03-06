81bb85764f090beb566ddfc32ce1bbf0716023f4
NSD <metall112@bk.ru>
2017-03-06 12:58:53 +0200

    end integration # fix db auth

--- src/main/java/NSD/NSDConstats.java
+++ src/main/java/NSD/NSDConstats.java
@@ -11,7 +11,7 @@ public class NSDConstats {
     public static String getDBURL() {
         if(DBURL == null){
 
-         DBURL = "jdbc:mysql://us-cdbr-iron-east-04.cleardb.net:3306/heroku_4574124234c67bc?reconnect=true";
+         DBURL = "jdbc:mysql://us-cdbr-iron-east-04.cleardb.net:3306/heroku_4574124234c67bc?reconnect=true&user=bae2125aeae8cb&password=0a4755709189264";
         }
 
         return DBURL;
