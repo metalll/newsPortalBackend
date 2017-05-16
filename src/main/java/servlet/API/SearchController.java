8a633e9df8686f9ea560c3418b7877880ab8553f
NSD <metall112@bk.ru>
2017-05-16 10:10:37 +0300

    update submodule

---
+++ src/main/java/servlet/API/SearchController.java
@@ -0,0 +1,80 @@
+package servlet.API;
+
+import DBControllers.DBService;
+import com.google.gson.Gson;
+import model.Service;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by NSD on 16.05.17.
+ */
+@WebServlet(name= "SearchController", urlPatterns = {"/API/Search"} )
+public class SearchController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+
+        Map<String,String[]> paramMap = req.getParameterMap();
+
+        String query = null;
+
+        try{
+            query = paramMap.get("q")[0];
+        }
+        catch (Exception e){
+            query = null;
+        }
+
+        if(query == null){
+
+            // TODO: 16.05.17 return status 404 error query;
+
+            return;
+        }
+
+
+
+
+        List<Service> queryServicesList = DBService.getInstance().QueryAll();
+
+        for(Service item : queryServicesList){
+
+
+            if(!item.getHeaderText().toLowerCase().contains(query.toLowerCase())){
+
+                queryServicesList.remove(item);
+
+            }
+
+        }
+
+
+        Gson gson = new Gson();
+        String result = gson.toJson(queryServicesList);
+
+
+
+
+        resp.setStatus(HttpServletResponse.SC_OK);
+        PrintWriter out = resp.getWriter();
+
+        out.write(result);
+        out.flush();
+        out.close();
+
+
+
+
+    }
+}
