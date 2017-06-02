48352e3a85a4315f19faffce35982c7bdc2e5dd3
NSD <metall112@bk.ru>
2017-06-02 10:07:54 +0300

    ready do deploy

--- src/main/java/servlet/API/MailController.java
+++ src/main/java/servlet/API/MailController.java
@@ -20,23 +20,36 @@ public class MailController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+try {
+    Map<String, String[]> paramMap = req.getParameterMap();
 
-        Map<String,String[]> paramMap = req.getParameterMap();
-
-        String toEmail = paramMap.get("email_dest")[0];
-        String message = paramMap.get("message")[0];
-
-
-        String theme = null;
-        try {
-            theme = paramMap.get("theme")[0];
-        }catch (NullPointerException e){
-            theme=null;
-        }
+    String toEmail = paramMap.get("email_dest")[0];
+    String message = paramMap.get("message")[0];
 
 
-        MailSender.getInstance().send(theme==null?"Ниуккцпб":theme,message,"linuxoid6666@gmail.com",toEmail);
+    String theme = null;
+    try {
+        theme = paramMap.get("theme")[0];
+    } catch (NullPointerException e) {
+        theme = null;
+    }
 
+
+    MailSender.getInstance().send(theme == null ? "Ниуккцпб" : theme, message, "linuxoid6666@gmail.com", toEmail);
+}catch (Exception e){
+    // if basic sender dont work deserialise form
+    Map<String,String[]> paramMap = req.getParameterMap();
+
+
+
+    String icon_prefix = paramMap.get("icon_prefix")[0];
+    String icon_telephone = paramMap.get("icon_telephone")[0];
+    String call_theme_select = paramMap.get("call_theme_select")[0];
+
+
+
+
+}
 
 
 
