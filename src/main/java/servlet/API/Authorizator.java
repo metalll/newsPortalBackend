db1a618a1e4124e96812a03286352d1e5d13992f
metalll <metall112@bk.ru>
2017-08-09 02:37:24 +0300

    add news backend

---
+++ src/main/java/servlet/API/Authorizator.java
@@ -0,0 +1,48 @@
+package servlet.API;
+
+import sun.net.httpserver.HttpServerImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * Created by nsd on 09.08.17.
+ */
+
+@WebServlet(name= "Auth", urlPatterns = {"/API/Auth"} )
+public class Authorizator extends HttpServlet{
+
+    public static String UUID = null;
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String pass = null;
+        try {
+
+
+        pass =  req.getParameter("pass");
+        }catch (Exception e){
+
+
+
+        }
+
+
+        if(pass!=null){
+
+            if(pass.equals("QazWsx321")) {
+
+                this.UUID = java.util.UUID.randomUUID().toString();
+
+                req.getSession().setAttribute("auth",UUID);
+            }
+        }
+
+    }
+}
